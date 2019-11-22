e0442d0481d165dc874c893227a412b48606f8f3
verdomazeg <bertaruh.rindoe@gmail.com>
2019-11-22 14:32:47 +0700

    Fix issue

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 module.exports = {
-	user          : process.env.NODE_ORACLEDB_USER || "mobile_inspection",
-	password      : process.env.NODE_ORACLEDB_PASSWORD || "mobile_inspection",
+	user          : process.env.NODE_ORACLEDB_USER || "db_user",
+	password      : process.env.NODE_ORACLEDB_PASSWORD || "db_pass",
 	connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "10.20.1.111:1521/tapapps",
 };
